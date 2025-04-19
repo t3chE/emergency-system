dfa91da5f1899c444017a3cc3a91cf3f7a44de6d
t3chE <t0dtym3@gmail.com>
2025-04-19 13:22:40 +0100

    File name changes

--- app/main.py
+++ app/main.py
@@ -1,4 +1,8 @@
+from app.incidents import incident
 from app.management import management
+from app.priorities import priority
+from app.resources import resource
+
 if __name__ == "__main__":
     ems = management() # Import the management module from the app package
     ems.run() 
