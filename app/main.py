4844b42bc0e52e041a924647be329e17066c2403
t3chE <t0dtym3@gmail.com>
2025-04-19 12:57:59 +0100

    Notes

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
 from app.management import management
 if __name__ == "__main__":
-    ems = management()
-    ems.run()
+    ems = management() # Import the management module from the app package
+    ems.run() 
