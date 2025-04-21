3c910678cccdf96ed0d9285e109c5510a53fa6f9
t3chE <t0dtym3@gmail.com>
2025-04-21 18:02:25 +0100

    Priority enum defined

--- app/priorities/emerg_priority.py
+++ app/priorities/emerg_priority.py
@@ -1,7 +1,22 @@
 from enum import Enum
+
 class Priority(Enum):
-    CATEGORY_1 = "category_1"
-    CATEGORY_2 = "category_2"
-    CATEGORY_3 = "category_3"
-    CATEGORY_4 = "category_4"
+    CAT_1 = "cat_1"
+    CAT_2 = "cat_2"
+    CAT_3 = "cat_3"
+    CAT_4 = "cat_4"
    
+    def __lt__(self, other):
+        """ Define less than for priority comparison (CAT_1 > CAT_2 > ...) """
+
+        if self.__class__ is other.__class__:
+            return list(self.__class__).index(self) < list(other.__class__).index(other)
+        return NotImplemented 
+
+    def __gt__(self, other):
+        """ Define greater than for priority comparison """
+
+        if self.__class__ is other.__class__:
+            return list(self.__class__).index(self) > list(other.__class__).index(other)
+        return NotImplemented
+       
